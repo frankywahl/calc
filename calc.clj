63761ea23cd4ea8f0bee8573e12bbcb4d96393fc
Myles Byrne <myles@myles.id.au>
2016-05-09 19:01:46 -0700

    Tokenizer done in Clojure

--- calc.clj
+++ calc.clj
@@ -9,7 +9,7 @@
  (set (conj (map str (range 0 10)) ".")))
 
 (def operators #{"+" "-"})
-(def parens {"(" :open, ")" :closed})
+(def parens {"(" :open, ")" :close})
 ;(defn parse_token)
 
 
@@ -56,6 +56,48 @@
   (if (empty? buffer)
    [token-list ""]
    [(conj token-list (parse-token buffer)) ""])))
+
+(def tokenize-examples
+ [
+   { :given "0", :expect [0M] },
+   { :given "1 + 1", :expect [1M, :+, 1M] },
+   ; todo: negative number tokens
+   ; { given: "1+-1", expect: [1, :+, -1] },
+   { :given "1 - 1", :expect [1M, :-, 1M] },
+   { :given "1 + (1 + 1)", :expect [1M, :+, :open, 1M, :+, 1M, :close]}])
+
+(defn chars-without-spaces
+ [s]
+ (as->
+   s val
+   (seq val)
+   (filter #(not= \space %1) val)
+   (map str val)))
+
+
+(defn tokenize
+ [s]
+
+ (let [initial-memo (tokenize-step)]
+  (let [last-memo (reduce tokenize-step initial-memo (chars-without-spaces s))]
+   (first (tokenize-step last-memo)))))
+
+
+(deftest tokenize-test
+ (doseq [example tokenize-examples]
+  (is (= (:expect example) (tokenize (:given example))))))
+
+
+; def tokenize(str)
+;    chrs_without_spaces =
+;      str
+;      .chars
+;      .reject(&method(:space?))
+;
+;    reduce(chrs_without_spaces, method(:tokenize_step))
+;      .first
+;  end
+
 
 
 (deftest tokenize-step-test
